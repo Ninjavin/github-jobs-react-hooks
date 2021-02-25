bec86170f1cfdc52d06d206b5e835796100e0faa
Ninjavin <vinjain2006@gmail.com>
2021-02-25 10:17:27 +0530

    Backup

--- README.md
+++ README.md
@@ -68,3 +68,14 @@ This section has moved here: [https://facebook.github.io/create-react-app/docs/d
 ### `yarn build` fails to minify
 
 This section has moved here: [https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify](https://facebook.github.io/create-react-app/docs/troubleshooting#npm-run-build-fails-to-minify)
+
+# Learnings
+
+* Built using React Hooks Completely
+> React Hooks allows to create functional components
+	+ From Class to () => {...}
+	+ Convert state+setState(...) to useState(...)
+* `useReducer` state
+* `useEffect` Hook
+* *System Limit No of watchers problem faced* - `sudo nano /etc/sysctl.conf` then increase the limit `fs.inotify.max_user_watches=50000`, check again using `sudo sysctl -p`
+* Axios Cancel Token
--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import useFetchJobs from './useFetchJobs';
+import { Container } from 'react-bootstrap';
+import Job from './Job';
+import JobsPagination from './JobsPagination';
+import SearchForm from './SearchForm';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+	const [params, setParams] = useState({})
+	const [page, setPage] = useState(1)
+	const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
+
+	function handleParamChange(e){
+		const param = e.target.name
+		const value = e.target.value
+		setPage(1)
+		setParams(prevParams => {
+			return { ...prevParams, [param]: value}
+		})
+	}
+	return (
+		<Container className="my-4">
+			<h3 className="mb-4">GitHub Jobs</h3>
+			<SearchForm params={params} onParamChange={handleParamChange} />
+			<JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+			{ loading && <h3>Loading...</h3>}
+			{ error && <h3>Error</h3>}
+			{ jobs.map(job => {
+				return <Job key={job.id} job={job}></Job>
+			})} 
+			<JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+		</Container>
+	)
 }
 
 export default App;
